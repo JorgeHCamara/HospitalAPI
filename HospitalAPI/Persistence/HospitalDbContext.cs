aeb431bee17b16290590e763b36848891c7801a6
JorgeHCamara <jorgehcamara@hotmail.com>
2023-09-06 17:19:13 -0300

    Adicionar arquivos de projeto.

---
+++ HospitalAPI/Persistence/HospitalDbContext.cs
@@ -0,0 +1,13 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace HospitalAPI.Persistence
+{
+    public class HospitalDbContext : DbContext
+    {
+        public HospitalDbContext(DbContextOptions<HospitalDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
